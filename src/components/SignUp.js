1569a9f3c4c71c3800c2e5d19e00a8b5a2a8d8f1
Miguel Pardo <migsub77@gmail.com>
2016-12-14 17:40:16 -0800

    signup form validation completed

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,35 +7,77 @@ export default class SignUp extends Component {
   constructor() {
     super();
     this.submitForm = this.submitForm.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.state = {
+      errors: '',
+    }
   }
+
+  validateForm(values) {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = 'Please enter an email.';
+    }
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+      errors.password = 'Please enter a password.';
+    }
+    if (!values.confirmPassword) {
+      errors.confirmPassword = 'Please enter a password confirmation.';
+    }
+    if (values.password !== values.confirmPassword) {
+      errors.password = 'Passwords do not match';
+    }
+    console.log('errors:', errors);
+    this.setState({
+      errors: errors,
+    });
+
+    return errors;
+  };
 
   submitForm (e) {
     e.preventDefault();
     const target = e.target;
     const userInfo = {
-      userName: target.userName.value,
+      // userName: target.userName.value,
       email: target.email.value,
       password: target.password.value,
       confirmPassword: target.confirmPassword.value,
     }
-    // console.log('userInfo:', userInfo);
 
-    signUpUser(userInfo);
+    let createAccount = new Promise ((resolve, reject) => {
+      resolve(this.validateForm(userInfo));
+    });
+    createAccount.then( res => {
+    let verified = !Object.keys(res).length ? true : false;
+    verified && signUpUser(userInfo);
+    })
+
+
+    // this.validateForm(userInfo)
+    // signUpUser(userInfo);
   }
 
   render() {
+    const { errors } = this.state;
     return(
       <div className="signUpFormContainer">
         <h2 className="text-center">Create an Account</h2>
         <form onSubmit={this.submitForm} className="signUpForm">
-          <label htmlFor="userName">Username</label>
-          <input type="text" name="userName" />
+          {/* <label htmlFor="userName">Username</label> */}
+          {/* <input type="text" name="userName" required/> */}
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" />
+          <p>{errors.email}</p>
+          <input type="email" name="email" required/>
           <label htmlFor="password">Password</label>
-          <input type="text" name="password"/>
+          <input type="text" minLength="6" name="password" required />
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="text" name="confirmPassword"/>
+          <input type="text" minLength="6" name="confirmPassword" required />
           <button>Create Account</button>
         </form>
       </div>
