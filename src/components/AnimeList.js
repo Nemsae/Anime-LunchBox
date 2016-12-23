4dc7dad92358ad7338e315ea88221bb571ebb2fd
Miguel Pardo <migsub77@gmail.com>
2016-12-22 18:56:27 -0800

    fixed favorites adding of anime

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -37,51 +37,58 @@ export default class AnimeList extends Component {
   }
 
 
-render () {
-  let { currAnime, background } = this.state;
-  let animeList = this.props.animeList || [];
-  let currPage = this.props.currPage;
-  return (
-    <div className="compContainer">
-      <AnimeModal anime={currAnime} background={background} currPage={currPage} />
-      {animeList.map(anime => {
-        return (
-          <div key={anime.id} className='encloser' onClick={() => this.setCurrAnime(anime)} data-toggle='modal' data-target={`.bs-example-modal-md`}>
-            <div className='animeSquare'>
-              <div className='picContainer'>
-                <img src={anime.attributes.posterImage.original || ''} className='pic' />
+  render () {
+    let { currAnime, background } = this.state;
+    var animeList = this.props.animeList || {};
+    let currPage = this.props.currPage;
+    console.log('animeList:', animeList);
+    return (
+      <div className="compContainer">
+        <AnimeModal anime={currAnime} background={background} currPage={currPage} />
+        {
+          Object.keys(animeList) &&
+          Object.keys(animeList || {}).map(item => {
+            const anime = animeList[item];
+            console.log('anime23535353235:', anime);
+            return (
+              <div key={anime.id} className='encloser' onClick={() => this.setCurrAnime(anime)} data-toggle='modal' data-target={`.bs-example-modal-md`}>
+                <div className='animeSquare'>
+                  <div className='picContainer'>
+                    <img src={anime.attributes.posterImage.original || ''} className='pic' />
+                  </div>
+                  <h4>{anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp}</h4>
+                </div>
               </div>
-              <h4>{anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp}</h4>
-            </div>
-          </div>
-        )
-      })}
-    </div>
-  )
+            )
+          })
+        }
+
+      </div>
+    )
   }
 }
 
- // render () {
- //    let animeList = this.props.animeList;
- //    let AnimeList = [];
- //    if (typeof animeList === 'object') {
- //      let keys = Object.keys(animeList) || [];
- //      AnimeList = keys.map(anime => {
- //        console.log('animeList:', animeList);
- //        return (
- //          <div key={animeList[anime].id} className='encloser' onClick={() => this.setCurrAnime(animeList[anime])} data-toggle='modal' data-target={`.bs-example-modal-md`}>
- //            <div className='animeSquare'>
- //              <div className='picContainer'>
- //                <img src={animeList.attributes.posterImage ? animeList.attributes.posterImage.original : ''} className='pic' />
- //              </div>
- //              <h4>{animeList[anime].title}</h4>
- //            </div>
- //          </div>
- //        );
- //      });
- //    }
- //    let { currAnime, background } = this.state;
- //    let currPage = this.props.currPage;
+// render () {
+//    let animeList = this.props.animeList;
+//    let AnimeList = [];
+//    if (typeof animeList === 'object') {
+//      let keys = Object.keys(animeList) || [];
+//      AnimeList = keys.map(anime => {
+//        console.log('animeList:', animeList);
+//        return (
+//          <div key={animeList[anime].id} className='encloser' onClick={() => this.setCurrAnime(animeList[anime])} data-toggle='modal' data-target={`.bs-example-modal-md`}>
+//            <div className='animeSquare'>
+//              <div className='picContainer'>
+//                <img src={animeList.attributes.posterImage ? animeList.attributes.posterImage.original : ''} className='pic' />
+//              </div>
+//              <h4>{animeList[anime].title}</h4>
+//            </div>
+//          </div>
+//        );
+//      });
+//    }
+//    let { currAnime, background } = this.state;
+//    let currPage = this.props.currPage;
 
 //     return (
 //       <div className='compContainer'>
--- src/components/AnimeModal.js
+++ src/components/AnimeModal.js
@@ -39,110 +39,117 @@ export default class AnimeModal extends Component {
   _animeDetails (anime, addType, uid) {
     console.log('ANIME IN DETAILSSSSS:', anime);
     firebaseDb.ref('users/' + `${uid}/` + `${addType}/` + `${anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp}/`).update({
-      id: anime.id,
-      title: anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp,
-      episode_count: anime.attributes.episodeCount,
-      cover_image: anime.attributes.posterImage.original,
-      synopsis: anime.attributes.synopsis,
-      show_type: anime.attributes.showType,
-      started_airing: anime.attributes.startDate,
-      finished_airing: anime.attributes.endDate,
-      community_rating: Math.round(anime.attributes.averageRating * 100) / 100,
-      age_rating: anime.attributes.ageRating,
-      // genres: JSON.stringify(anime.attributes.genres)
-    });
-  }
+        id: anime.id,
+        attributes: {
+          titles: {
+            en: anime.attributes.titles.en || '',
+            en_jp: anime.attributes.titles.en_jp || '',
+          },
+          episodeCount: anime.attributes.episodeCount,
+          posterImage: {
+            original: anime.attributes.posterImage.original,
+          },
+          synopsis: anime.attributes.synopsis,
+          showType: anime.attributes.showType,
+          startDate: anime.attributes.startDate,
+          endDate: anime.attributes.endDate,
+          averageRating: Math.round(anime.attributes.averageRating * 100) / 100,
+          ageRating: anime.attributes.ageRating,
+          // genres: JSON.stringify(anime.attributes.genres)
+        }
+  });
+}
 
-  _addFavorite (anime) {
-    var show = anime;
-    let { initStatus } = this.state;
-    if (initStatus) {
-      let { uid } = initStatus;
-      this._animeDetails(show,'Favorites', uid);
-    }
-  }
-
-  _addToWatch (anime) {
-    var show = anime;
-    let { initStatus } = this.state;
-    if (initStatus) {
-      let { uid } = initStatus;
-      this._animeDetails(show,'Watchlist', uid);
+_addFavorite (anime) {
+  var show = anime;
+  let { initStatus } = this.state;
+  if (initStatus) {
+    let { uid } = initStatus;
+    this._animeDetails(show,'Favorites', uid);
   }
 }
 
-  render () {
+_addToWatch (anime) {
+  var show = anime;
+  let { initStatus } = this.state;
+  if (initStatus) {
+    let { uid } = initStatus;
+    this._animeDetails(show,'Watchlist', uid);
+  }
+}
 
-    let { anime, background, currPage } = this.props;
-    console.log('anime:', anime);
-    let num = Math.floor(Math.random() * background.length);
-    let divStyle = {
-      backgroundImage: background.length && `url(${background[num].images.original.url})`
-    };
-    return (
-      <div className={`modal fade bs-example-modal-md firstLevelModal`} id="animeModalMain" tabIndex='-1' role='dialog' aria-labelledby='mySmallModalLabel'>
-        <div className='modal-dialog modal-md secondLevelModal' role='document'>
-          <div className='modal-content thirdLevelModal'>
-            <div className='modalPicContainer fourthLevelModal' >
-              {
-                Object.keys(anime).length &&
-                  <div className='animeInfoContainer'>
-                    <h5 className="animeModalClose" data-dismiss="modal" target="firstLevelModal">X</h5>
-                    <div className='animeTitle text-center'>
-                      <img src={anime.attributes.posterImage.original || ''} alt='main pic' className='modalPic' />
-                      <h3 className='headings title'><b>{anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp}</b></h3>
-                    </div>
-                    { currPage === "search" ?
-                      <div className='btnContainer text-center'>
-                        <button className='btn btn-primary' onClick={this._addFavorite.bind(null, anime)}>Add to Favorites</button>
-                        <button className='btn btn-success' onClick={this._addToWatch.bind(null, anime)}>Add to WatchList</button>
-                      </div>
-                      :
-                      <div className="btnContainer text-center">
-                        <button className="btn btn-danger">Delete</button>
-                      </div>
-                    }
-                    <div className='animeInfo'>
-                      <h4>Type: {anime.attributes.showType}</h4>
-                      {/* <h4>Status: {anime.status}</h4> */}
-                      <h4>Episodes: {anime.attributes.episodeCount}</h4>
-                      <h4>Rating: {Math.round(anime.attributes.averageRating * 100) / 100}</h4>
-                      <h4>Rated: {anime.attributes.ageRating}</h4>
-                      <h4>Aired: {anime.attributes.startDate} - {anime.attributes.endDate}</h4>
-                    </div>
-                    <h4 className='summary'>Summary: {anime.attributes.synopsis}</h4>
-                    <div className='genreContainer'>
-                      <h4>Genres:</h4>
-                      {
-                        // anime.genres !== undefined && anime.genres.map((genre) => {
-                        //   console.log('genre.name:', genre.name);
-                        //   return (
-                        //     <h5 key={uuid()}>{genre.name}</h5>
-                        //   );
-                        // })
-                      }
-                    </div>
-                  </div>
+render () {
 
+  let { anime, background, currPage } = this.props;
+  console.log('anime:', anime);
+  let num = Math.floor(Math.random() * background.length);
+  let divStyle = {
+    backgroundImage: background.length && `url(${background[num].images.original.url})`
+  };
+  return (
+    <div className={`modal fade bs-example-modal-md firstLevelModal`} id="animeModalMain" tabIndex='-1' role='dialog' aria-labelledby='mySmallModalLabel'>
+      <div className='modal-dialog modal-md secondLevelModal' role='document'>
+        <div className='modal-content thirdLevelModal'>
+          <div className='modalPicContainer fourthLevelModal' >
+            {
+              Object.keys(anime).length &&
+              <div className='animeInfoContainer'>
+                <h5 className="animeModalClose" data-dismiss="modal" target="firstLevelModal">X</h5>
+                <div className='animeTitle text-center'>
+                  <img src={anime.attributes.posterImage.original || ''} alt='main pic' className='modalPic' />
+                  <h3 className='headings title'><b>{anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp}</b></h3>
+                </div>
+                { currPage === "search" ?
+                <div className='btnContainer text-center'>
+                  <button className='btn btn-primary' onClick={this._addFavorite.bind(null, anime)}>Add to Favorites</button>
+                  <button className='btn btn-success' onClick={this._addToWatch.bind(null, anime)}>Add to WatchList</button>
+                </div>
+                :
+                <div className="btnContainer text-center">
+                  <button className="btn btn-danger">Delete</button>
+                </div>
               }
-
-              {
-                Object.keys(anime).length &&
-                  <div className='playerContainer'
-                    style={divStyle}
-                  >
-                    <iframe allowFullScreen='allowFullScreen' id='player' type='text/html' width='640' height='390'
-                      src={anime.attributes.youtubeVideoId ? `https://www.youtube.com/embed?listType=search&list=${anime.attributes.youtubeVideoId}` : `https://www.youtube.com/embed?listType=search&list=${anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp} anime`}
-                      frameBorder='0'></iframe>
-                  </div>
-              }
-
-
+              <div className='animeInfo'>
+                <h4>Type: {anime.attributes.showType}</h4>
+                {/* <h4>Status: {anime.status}</h4> */}
+                <h4>Episodes: {anime.attributes.episodeCount}</h4>
+                <h4>Rating: {Math.round(anime.attributes.averageRating * 100) / 100}</h4>
+                <h4>Rated: {anime.attributes.ageRating}</h4>
+                <h4>Aired: {anime.attributes.startDate} - {anime.attributes.endDate}</h4>
+              </div>
+              <h4 className='summary'>Summary: {anime.attributes.synopsis}</h4>
+              <div className='genreContainer'>
+                <h4>Genres:</h4>
+                {
+                  // anime.genres !== undefined && anime.genres.map((genre) => {
+                  //   console.log('genre.name:', genre.name);
+                  //   return (
+                  //     <h5 key={uuid()}>{genre.name}</h5>
+                  //   );
+                  // })
+                }
+              </div>
             </div>
 
+          }
+
+          {
+            Object.keys(anime).length &&
+            <div className='playerContainer'
+              style={divStyle}
+              >
+                <iframe allowFullScreen='allowFullScreen' id='player' type='text/html' width='640' height='390'
+                src={anime.attributes.youtubeVideoId ? `https://www.youtube.com/embed?listType=search&list=${anime.attributes.youtubeVideoId}` : `https://www.youtube.com/embed?listType=search&list=${anime.attributes.titles.en ? anime.attributes.titles.en : anime.attributes.titles.en_jp} anime`}
+                frameBorder='0'></iframe>
+              </div>
+            }
+
+
           </div>
+
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
